07413abe0894791d728ce6e2937019f5f3a63c97
germanpro98 <germandelfino22@gmail.com>
2025-05-27 16:37:12 -0300

    Delete formulario.py

--- formulario.py
+++
@@ -1,7 +0,0 @@
-print("creando formulario")
-
-nombre = input("ingrese su nombre: ")
-edad = input("ingrese su edad: ")
-apellido = input("ingrese su apellido: ")
-print("los datos personales son: ", nombre, apellido, edad)
-# Se solicita al usuario que ingrese su nombre, apellido, edad y ciudad de residencia.
